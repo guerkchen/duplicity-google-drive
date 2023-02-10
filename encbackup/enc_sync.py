eb09a19c166d4cd0110ecb937c97e848543c5cff
Matze Wild <matthias@wildpeter.de>
2023-02-10 21:38:04 +0100

    started implementing relative paths

--- encbackup/enc_sync.py
+++ encbackup/enc_sync.py
@@ -31,8 +31,8 @@ LOG_LEVEL = logging.INFO
 TMP_PATH = "/tmp/ebfbf"
 
 # Runtime parameters (these can be changed by command line arguments or config file)
-src_folder = ""
-backup_folder = ""
+src_dir = ""
+backup_dir = ""
 password_file = ""
 config_file = ""
 delete = False
@@ -55,7 +55,7 @@ log_level_info = {
 class File_Entry(JSONListWizard, JSONFileWizard):
     size: int
     ctime: float
-    filename: str
+    rel_path: str
     enc_filename: str
     enc_size: int # with this size we can assure, that the backup files are not corrupted. We can use this number to check every run that all files are valid
 
@@ -63,18 +63,22 @@ class File_Entry(JSONListWizard, JSONFileWizard):
 ## FILE ENTRY METHODS ##
 ##############################################
 
-def get_file_entry(filename):
+def get_file_entry(abs_path, src_dir):
     # get additional informations for every file
-    size = os.path.getsize(filename) # size
-    ctime = os.path.getctime(filename) # last modified date
-    enc_filename = 'enc-' + sha256(filename.encode('utf-8')).hexdigest() + '.gpg' # create enc filename (by hashing)
-    file_entry =  File_Entry(size, ctime, filename, enc_filename, 0)
+    size = os.path.getsize(abs_path) # size
+    ctime = os.path.getctime(abs_path) # last modified date
+
+    # the file is referenced by it's relative path, starting at the src_dir. This way we can easily move the src_dir to another location
+    rel_path = os.path.relpath(abs_path, src_dir)
+
+    enc_filename = 'enc-' + sha256(rel_path.encode('utf-8')).hexdigest() + '.gpg' # create enc filename (by hashing the relative path)
+    file_entry =  File_Entry(size, ctime, rel_path, enc_filename, 0)
     logging.log(1, file_entry)
     return file_entry
 
-def get_folder_struc(src_folder, backup_folder):
+def get_folder_struc(src_dir, backup_dir):
     # get file list
-    backup_scan = os.scandir(backup_folder)
+    backup_scan = os.scandir(backup_dir)
     # I try to reduce the system calls to increase performance.
     # Especially the remote file systems tends to take a long time handling calls, so I ask as much as possible in one block
     # In the backup folder there is no folder structure, so one request gives all the answers
@@ -89,24 +93,25 @@ def get_folder_struc(src_folder, backup_folder):
     if MASTERFILE_NAME in backup_dict:
         del backup_dict[MASTERFILE_NAME]
 
-    return get_folder_struc_rec(src_folder, backup_dict), backup_dict
+    return get_folder_struc_rec(src_dir, backup_dict, src_dir), backup_dict
 
 # Hopefully there will be a method to scandirs recursive soon
 # os.walk is not useful since it doesn't deliver the files size and ctime
-def get_folder_struc_rec(rec_scan, backup_dict):
+def get_folder_struc_rec(rec_scan, backup_dict, src_dir):
     for file in os.scandir(rec_scan):
         if file.name.startswith('.'):
             # skip hidden files
             # we check this before we handle folders, so hidden folders will be skipped aswell
             continue
 
-        filename = file.path
+        abs_path = file.path
         if file.is_dir():
             # go deeper on folders
-            yield from get_folder_struc_rec(filename, backup_dict)
+            yield from get_folder_struc_rec(abs_path, backup_dict, src_dir)
             continue
 
-        enc_filename = 'enc-' + sha256(filename.encode('utf-8')).hexdigest() + '.gpg' # create enc filename (by hashing)
+        rel_path = os.path.relpath(abs_path, src_dir)
+        enc_filename = 'enc-' + sha256(rel_path.encode('utf-8')).hexdigest() + '.gpg' # create enc filename (by hashing)
         if enc_filename not in backup_dict:
             # no backup file found. This normally happens, when the file is not backuped yet
             # but it could also mean, that we lost the backuped file or it is corrupted.
@@ -116,14 +121,14 @@ def get_folder_struc_rec(rec_scan, backup_dict):
             enc_size = backup_dict[enc_filename]
             del backup_dict[enc_filename] # by deleting the entry at this point, we can use the remaining entries later to determine lost+found files in the backup directory
 
-        yield File_Entry(file.stat().st_size, file.stat().st_ctime, filename, enc_filename, enc_size)
+        yield File_Entry(file.stat().st_size, file.stat().st_ctime, rel_path, enc_filename, enc_size)
 
 ##############################################
 ## MASTERFILE READ AND WRITE ##
 ##############################################
 
 # we do a little Choreography to garantee, there is always a valid backup_master file available
-def write_masterfile(masterfile_path, masterfile_path_old, folder_struc, password_file):
+def write_masterfile(masterfile_abs_path, masterfile_abs_path_old, folder_struc, password_file):
     logging.debug("save masterfile to backup drive")
 
     tmp_file = os.path.join(TMP_PATH, MASTERFILE_NAME)
@@ -134,17 +139,17 @@ def write_masterfile(masterfile_path, masterfile_path_old, folder_struc, passwor
     command = ['gpg', '--symmetric', '--armor', '--batch', '--yes', '--passphrase-file', password_file, '-o', tmp_file]
     out = subprocess.check_output(command, input=text.encode('utf-8'))
 
-    if os.path.isfile(masterfile_path):
+    if os.path.isfile(masterfile_abs_path):
         # rename the current masterfile on the backup drive (if it exists)
         logging.log(5, "backup old masterfile")
-        os.rename(masterfile_path, masterfile_path_old)
+        os.rename(masterfile_abs_path, masterfile_abs_path_old)
 
     logging.log(5, "transfer new masterfile to backup drive")
-    os.rename(tmp_file, masterfile_path)
+    os.rename(tmp_file, masterfile_abs_path)
 
-    if os.path.isfile(masterfile_path_old):
+    if os.path.isfile(masterfile_abs_path_old):
         logging.log(5, "delete the old masterfile")
-        os.remove(masterfile_path_old)
+        os.remove(masterfile_abs_path_old)
 
 
 def read_masterfile(masterfile_path, masterfile_path_old, password_file):
@@ -210,7 +215,7 @@ def convert_into_dictionary(folder_struc):
 def main(argv):
     # DEFAULT CONFIG (can be changed by config file)
     global MASTERFILE_WRITE_TIME_SEC, MASTERFILE_NAME, LOG_LEVEL, TMP_PATH
-    global src_folder, backup_folder, password_file, config_file, delete, restore
+    global src_dir, backup_dir, password_file, config_file, delete, restore
 
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
@@ -241,8 +246,8 @@ def main(argv):
         LOG_LEVEL = log_level_info.get(config['CONSTANT']['LOG_LEVEL'], logging.ERROR)
         TMP_PATH = config['CONSTANT']['TMP_PATH']
 
-        src_folder = config['VARIABLE']['SRC_FOLDER']
-        backup_folder = config['VARIABLE']['BACKUP_FOLDER']
+        src_dir = config['VARIABLE']['SRC_FOLDER']
+        backup_dir = config['VARIABLE']['BACKUP_FOLDER']
         password_file = config['VARIABLE']['PASSWORD_FILE']
         delete = config.getboolean('VARIABLE', 'DELETE')
         restore = config.getboolean('VARIABLE', 'RESTORE')
@@ -258,14 +263,14 @@ def main(argv):
         if opt in ('-r', '--restore'):
             restore = True
         elif opt == "--src":
-            src_folder = arg
+            src_dir = arg
         elif opt == "--backup":
-            backup_folder = arg
+            backup_dir = arg
         elif opt == "--password":
             password_file = arg
 
     # some variables must be filled, either from the config file or the cmd line argument
-    if src_folder == "" or backup_folder == "" or password_file == "":
+    if src_dir == "" or backup_dir == "" or password_file == "":
         logging.error("parameters:")
         logging.error("--src=<src folder> [REQ]")
         logging.error("--backup=<backup folder> [REQ]")
@@ -301,7 +306,7 @@ def main(argv):
     Path(TMP_PATH).mkdir(parents=True, exist_ok=True)
 
     # read existing masterfile
-    masterfile_path = os.path.join(backup_folder, MASTERFILE_NAME)
+    masterfile_path = os.path.join(backup_dir, MASTERFILE_NAME)
     masterfile_path_old = masterfile_path + ".old"
     if os.path.isfile(masterfile_path):
         should_struct = read_masterfile(masterfile_path, masterfile_path_old, password_file)
@@ -315,14 +320,14 @@ def main(argv):
                 exit()
         else:
             if click.confirm('No ' + MASTERFILE_NAME + ' found, so I assume, there is no backup. Create a new one?', default=True):
-                Path(backup_folder).mkdir(parents=True, exist_ok=True)
+                Path(backup_dir).mkdir(parents=True, exist_ok=True)
                 # just creat an empty container, so the programm thinks no files are backuped yet
                 should_struct = Container[File_Entry]()
             else:
                 exit()
 
     # read existing src folder struct
-    is_struct, lost_found_backup_dict = get_folder_struc(src_folder, backup_folder)
+    is_struct, lost_found_backup_dict = get_folder_struc(src_dir, backup_dir)
 
     ###############################################
     ## DO THE BACKUP ##
@@ -344,12 +349,12 @@ def main(argv):
             write_masterfile(masterfile_path, masterfile_path_old, should_struct, password_file)
             masterfile_last_written = time.time()
 
-        if is_entry.filename not in should_dict:
+        if is_entry.rel_path not in should_dict:
             # file is not backuped yet
-            logging.debug("new file '" + is_entry.filename + "' (" + str(is_entry.size) + ") -> encrypt & backup")
+            logging.debug("new file '" + is_entry.rel_path + "' (" + str(is_entry.size) + ") -> encrypt & backup")
             new_files_counter += 1
 
-            is_entry.enc_size = encrypt_and_backup(is_entry.filename, backup_folder, is_entry.enc_filename, password_file)
+            is_entry.enc_size = encrypt_and_backup(is_entry.rel_path, backup_dir, is_entry.enc_filename, password_file)
             # it's crucial that we first finish the file copy and then we update the should_struct
             # otherwise, if the program is killed during the backup process, the one backup file is corrupted and will not be fixed
             # This is not completly true, since we use the size of the encrypted file to check if it is valid, but still this way is better
@@ -359,7 +364,7 @@ def main(argv):
         # we can now assume that a backup entry for this file exists
         # we mark the object as handled by deleting it from the should_dict. Not the should_struct(!!) which is written to the MASTERFILE later
         # when leaving this loop, the remaining entries in the should_dict are no longer existence in the src_folder, so we must decide, wether we bring them back or delete them from the backup aswell
-        should_entry = should_dict.pop(is_entry.filename)
+        should_entry = should_dict.pop(is_entry.rel_path)
 
         if is_entry.enc_filename != should_entry.enc_filename:
             logging.debug("algorithm for calculating the enc_filename has changed since last run:")
@@ -369,9 +374,9 @@ def main(argv):
             if should_entry.enc_filename in lost_found_backup_dict:
                 if should_entry.enc_size == lost_found_backup_dict[should_entry.enc_filename]: 
                     # if the size is still correct, we just rename the encrypted file
-                    logging.debug("backup file for '" + is_entry.filename + "' (" + str(is_entry.size) + ") must be renamed")
+                    logging.debug("backup file for '" + is_entry.rel_path + "' (" + str(is_entry.size) + ") must be renamed")
                     corrupt_files_counter += 1
-                    command.run(['mv', os.path.join(backup_folder, should_entry.enc_filename), os.path.join(backup_folder, is_entry.enc_filename)])
+                    command.run(['mv', os.path.join(backup_dir, should_entry.enc_filename), os.path.join(backup_dir, is_entry.enc_filename)])
                     del lost_found_backup_dict[should_entry.enc_filename] # the file is no longer lost+found
                     should_entry.enc_filename = is_entry.enc_filename # update the filename
                     continue # file is handled
@@ -385,20 +390,20 @@ def main(argv):
         if is_entry.ctime != should_entry.ctime or is_entry.size != should_entry.size or is_entry.enc_size != should_entry.enc_size:
             if is_entry.enc_size != should_entry.enc_size:
                 # enc file is corrupt
-                logging.debug("corrupted file '" + is_entry.filename + "' (" + str(is_entry.size) + ") -> encrypt & backup")
+                logging.debug("corrupted file '" + is_entry.rel_path + "' (" + str(is_entry.size) + ") -> encrypt & backup")
                 logging.debug(str(is_entry.enc_size) + " != " + str(should_entry.enc_size))
                 corrupt_files_counter += 1
             else:
                 # file content has been changed
-                logging.debug("change in file '" + is_entry.filename + "' (" + str(is_entry.size) + ") -> encrypt & backup")
+                logging.debug("change in file '" + is_entry.rel_path + "' (" + str(is_entry.size) + ") -> encrypt & backup")
                 changed_files_counter += 1
 
-            should_entry.enc_size = encrypt_and_backup(is_entry.filename, backup_folder, should_entry.enc_filename, password_file)
+            should_entry.enc_size = encrypt_and_backup(is_entry.rel_path, backup_dir, should_entry.enc_filename, password_file)
             should_entry.ctime = is_entry.ctime # I hope, should_entry is a pointer to the original entry in should_struct
             should_entry.size = is_entry.size # I hope, should_entry is a pointer to the original entry in should_struct
             continue # file is handled
 
-        logging.debug("unchanged file '" + is_entry.filename + "' (" + str(is_entry.size) + ") -> relax")
+        logging.debug("unchanged file '" + is_entry.rel_path + "' (" + str(is_entry.size) + ") -> relax")
         neutral_files_counter += 1
 
     ###############################################
@@ -418,7 +423,7 @@ def main(argv):
         if restore:
             # lets bring it back
             logging.debug("found lost file '" + should_entry.filename + "' (" + str(should_entry.size) + ") -> decrypt & restore")
-            decrypt_and_restore(should_entry.filename, backup_folder, should_entry.enc_filename, password_file)
+            decrypt_and_restore(should_entry.filename, backup_dir, should_entry.enc_filename, password_file)
             # we dont need to restore data in the src_struct, since it will not be saved anywhere.
             # but we are required to update the ctime in the backup_struct, otherwise this file will be backuped next time, eventhough it's not necessary.
             # size should not be changed
@@ -428,7 +433,7 @@ def main(argv):
 
         if delete:
             # delete the backup file
-            enc_filename = os.path.join(backup_folder, should_entry.enc_filename)
+            enc_filename = os.path.join(backup_dir, should_entry.enc_filename)
             logging.debug("found old file '" + should_entry.filename + "' -> delete backup file")
             os.remove(enc_filename)
             should_struct.remove(should_entry) # since the encrypted file is deleted, the backup_entry must be deleted, too.
@@ -443,7 +448,7 @@ def main(argv):
     for lost_found_backup_entry in lost_found_backup_dict:
         lost_found_files_counter +=1
 
-        lost_found_filename = os.path.join(backup_folder, lost_found_backup_entry)
+        lost_found_filename = os.path.join(backup_dir, lost_found_backup_entry)
         if os.path.isfile(lost_found_filename): # this makes the code more robust
             logging.debug("delete lost+found file '" + lost_found_backup_entry + "'")
             os.remove(lost_found_filename)
